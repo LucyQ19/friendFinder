2c65b49837aeb06a2093768cb6793761e92aee8b
LucyQ19 <lucyq19@gmail.com>
2019-05-18 22:41:56 -0500

    added content to server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const apiRoutes = require("./app/routing/apiRoutes.js");
+const htmlRoutes = require("./app/routing/htmlRoutes.js");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+
+    console.log(`App is listening on PORT ${PORT}`);
+
+});
