183e60e7e9c2e65a151ba5b5ba87e2bc84fd79ec
LucyQ19 <lucyq19@gmail.com>
2019-05-18 22:37:02 -0500

    added content to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+const friends = [
+    {
+        name: "Buffy",
+        photo: "https://media.giphy.com/media/Xw1pNLan8ma3K/giphy.gif",
+        scores: [3, 1, 2, 4, 5, 2, 5, 3, 2, 5]
+    },
+    {
+        name: "Angel",
+        photo: "https://media.giphy.com/media/3vJizKG7cZYvm/giphy.gif", 
+        scores: [2, 1, 4, 5, 3, 2, 3, 2, 1, 3]
+    },
+    {
+        name: "Willow",
+        photo: "https://media.giphy.com/media/fQdNfaQ4OkcAo/giphy.gif",
+        scores: [1, 2, 5, 4, 3, 2, 3, 4, 1, 2]
+    },
+    {
+        name: "Xander",
+        photo: "https://media.giphy.com/media/E94REluJhW05G/giphy.gif",
+        scores: [4, 5, 3, 2, 2, 1, 4, 5, 5, 2]
+    },
+    {
+        name: "Cordelia",
+        photo: "https://media.giphy.com/media/xrb60V4E9DjbO/giphy.gif",
+        scores: [5, 1, 1, 4, 1, 1, 5, 2, 1, 3]
+    },
+    {
+        name: "Spike",
+        photo: "https://media.giphy.com/media/hhtxFi9SjKeHu/giphy.gif",
+        scores: [4, 3, 4, 4, 1, 5, 4, 3, 5, 4]
+    }
+    ];
+    
+    module.exports = friends;
