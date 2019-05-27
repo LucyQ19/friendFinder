f8d232e217f05fd3fe96bf391f8119ae85e5504f
LucyQ19 <lucyq19@gmail.com>
2019-05-27 10:26:36 -0500

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,32 +2,32 @@ const friendsData = [
     {
         name: "Buffy",
         photo: "https://media.giphy.com/media/Xw1pNLan8ma3K/giphy.gif",
-        scores: [3, 1, 2, 4, 5, 2, 5, 3, 2, 5]
+        scores: [25, 1, 10, 50, 90, 10, 90, 25, 10, 90]
     },
     {
         name: "Angel",
         photo: "https://media.giphy.com/media/3vJizKG7cZYvm/giphy.gif", 
-        scores: [2, 1, 4, 5, 3, 2, 3, 2, 1, 3]
+        scores: [10, 1, 50, 90, 25, 10, 25, 10, 1, 25]
     },
     {
         name: "Willow",
         photo: "https://media.giphy.com/media/fQdNfaQ4OkcAo/giphy.gif",
-        scores: [1, 2, 5, 4, 3, 2, 3, 4, 1, 2]
+        scores: [1, 10, 90, 50, 25, 10, 25, 50, 1, 10]
     },
     {
         name: "Xander",
         photo: "https://media.giphy.com/media/E94REluJhW05G/giphy.gif",
-        scores: [4, 5, 3, 2, 2, 1, 4, 5, 5, 2]
+        scores: [50, 90, 25, 10, 10, 1, 50, 90, 90, 10]
     },
     {
         name: "Cordelia",
         photo: "https://media.giphy.com/media/xrb60V4E9DjbO/giphy.gif",
-        scores: [5, 1, 1, 4, 1, 1, 5, 2, 1, 3]
+        scores: [90, 1, 1, 50, 1, 1, 90, 10, 1, 25]
     },
     {
         name: "Spike",
         photo: "https://media.giphy.com/media/hhtxFi9SjKeHu/giphy.gif",
-        scores: [4, 3, 4, 4, 1, 5, 4, 3, 5, 4]
+        scores: [50, 25, 50, 50, 1, 90, 50, 25, 90, 1]
     }
     ];
     
